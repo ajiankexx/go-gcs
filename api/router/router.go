54e774d37773156db8ccb1535c0bc4a3134ba47b
ajiankexx <ajiankex@163.com>
2025-07-03 16:45:02 +0800

    feat: support CORS(Cross-Origin Resource Sharing)

--- api/router/router.go
+++ api/router/router.go
@@ -2,10 +2,13 @@ package router
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/gin-contrib/cors"
 )
 
 func RouterSetup() *gin.Engine {
 	r := gin.Default()
+	r.Use(cors.Default())
+
 	v1 := r.Group("/api/v1")
 	{
 		SetupUserRoutes(v1)
