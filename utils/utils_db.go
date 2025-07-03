ca3f0169edd0edbeaedc277bf631d90bab896110
ajiankexx <ajiankex@163.com>
2025-07-03 16:46:22 +0800

    fix: change way to manage postgres

--- utils/utils_db.go
+++ utils/utils_db.go
@@ -4,18 +4,50 @@ import(
 	"log"
 
 	"github.com/jackc/pgx/v5"
+	"github.com/redis/go-redis/v9"
 )
 
-var dbConn *pgx.Conn
+var (
+	pgConn *pgx.Conn
+	redisConn *redis.Client
+)
 
-func InitDB(dsn string) {
+func InitDB() {
 	var err error
-	dbConn, err = pgx.Connect(context.Background(), dsn)
+	pgConn, err = pgx.Connect(context.Background(), "postgres://admin:1234@localhost:5432/gcs_db")
 	if err != nil {
-		log.Fatal("Unable to connect database: %v", err)
+		log.Fatalf("Unable to connect : %v", err)
+	}
+}
+
+func InitRedis() {
+	redisConn = redis.NewClient(&redis.Options{
+		Addr: "localhost:6379",
+		Password: "",
+		DB: 0,
+	})
+
+	if err := redisConn.Ping(context.Background()).Err(); err != nil {
+		log.Fatalf("Unable to connect redis: %v", err)
 	}
 }
 
 func GetDBConn() *pgx.Conn {
-	return dbConn
+	if pgConn == nil {
+		InitDB()
+	}
+	if pgConn == nil {
+		log.Fatal("Postgres not initialized")
+	}
+	return pgConn
+}
+
+func GetRedisConn() *redis.Client {
+	if redisConn == nil {
+		InitRedis()
+	}
+	if redisConn == nil {
+		log.Fatal("Redis not initialized")
+	}
+	return redisConn
 }
