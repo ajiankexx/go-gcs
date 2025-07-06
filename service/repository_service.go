41ad4c2c1260b0c4c207740afd2c996ad48bbe61
ajiankexx <ajiankex@163.com>
2025-07-06 17:05:21 +0800

    bug(Repository): generate Repository's link when create Repository
    Before this commit, when create repository, it didn't generate
    repository's https url and ssh url. However, both are asked to be not
    null in database. This leads to create repository fail.

--- service/repository_service.go
+++ service/repository_service.go
@@ -4,28 +4,42 @@ import (
 	"go-gcs/appError"
 	"go-gcs/dao"
 	"go-gcs/model"
+	"go-gcs/utils"
 
 	"context"
+
+	"github.com/jinzhu/copier"
 )
 
 type RepoService struct {
-	DAO *dao.RepoDB
+	RepoDAO *dao.RepoDB
+	UserDAO *dao.UserDB
 }
 
-func (r *RepoService) CreateRepo(ctx context.Context, req *model.CreateRepoDTO, userId string) (error) {
-	exist, _ := r.DAO.IsExists(ctx, req.RepoName, userId)
+// store user and repo infomation may reduce complexity
+func (r *RepoService) CreateRepo(ctx context.Context, req *model.CreateRepoDTO, userId int64) (error) {
+	exist, _ := r.RepoDAO.IsExists(ctx, req.RepoName, userId)
 	if exist {
 		return appError.ErrorRepoAlreadyExist
 	}
-	err := r.DAO.CreateRepo(ctx, req, userId)
+	var repoDO *model.RepoDO
+	err := copier.Copy(repoDO, req)
+	if err != nil {
+		return err
+	}
+	repoDO.User_id = userId
+	user, _ := r.UserDAO.GetUserByID(ctx, userId)
+	repoDO.Https_url = utils.GenerateHttpURL("localhost", 1234, user.Username, req.RepoName)
+	repoDO.Ssh_url = utils.GenerateSshURL("localhost", 1234, user.Username, req.RepoName)
+	err = r.RepoDAO.CreateRepo(ctx, repoDO, userId)
 	return err
 }
 
-func (r *RepoService) UpdateRepo(ctx context.Context, req *model.UpdateRepoDTO, userId string) (error) {
-	exist, _ := r.DAO.IsExists(ctx, req.RepoName, userId)
+func (r *RepoService) UpdateRepo(ctx context.Context, req *model.UpdateRepoDTO, userId int64) (error) {
+	exist, _ := r.RepoDAO.IsExists(ctx, req.RepoName, userId)
 	if !exist {
 		return appError.ErrorRepoNotExist
 	}
-	err := r.DAO.UpdateRepo(ctx, req, userId)
+	err := r.RepoDAO.UpdateRepo(ctx, req, userId)
 	return err
 }
---
+++ utils/utils_repository.go
@@ -0,0 +1,13 @@
+package utils
+
+import "fmt"
+
+func GenerateHttpURL(host string, port int, username string,
+	repoName string) string {
+	return fmt.Sprintf("https://%s:%d%s%s.git", host, port, username, repoName)
+}
+
+func GenerateSshURL(host string, port int, username string,
+	repoName string) string {
+	return fmt.Sprintf("git@%s:%d%s/%s.git", host, port, username, repoName)
+}
