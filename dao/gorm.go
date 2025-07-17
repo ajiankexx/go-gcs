923c9abfbafcb36da29d8725e393a82361bc1542
ajiankexx <ajiankex@163.com>
2025-07-17 16:32:21 +0800

    feat: support transaction in gorm

--- dao/gorm.go
+++ dao/gorm.go
@@ -2,19 +2,52 @@ package dao
 
 import (
 	"context"
+	"errors"
 	"go-gcs/model"
+	"runtime/debug"
 
-	"gorm.io/gorm"
 	"go.uber.org/zap"
+	"gorm.io/gorm"
 )
 
 type UserDB struct {
 	DB *gorm.DB
 }
 
-func (r *UserDB) GetUserByUserId(ctx context.Context, userId int64) (*model.UserDO, error) {
+func (r *UserDB) WithTransaction(ctx context.Context, fn func(tx *gorm.DB) error) error {
+	tx := r.DB.Begin()
+	if tx.Error != nil {
+		zap.L().Error("try to get transaction connection failed")
+		return tx.Error
+	}
+	defer func() {
+		if r := recover(); r != nil {
+			zap.L().Error("recover from panic",
+				zap.Any("panic", r),
+				zap.ByteString("stack", debug.Stack()),
+			)
+			tx.Rollback()
+			zap.L().Info("rollback successful")
+			panic(r)
+		}
+	}()
+	if err := fn(tx); err != nil {
+		zap.L().Error("transaction execute failed", zap.Error(err))
+		tx.Rollback()
+		zap.L().Info("rollback successful")
+		return err
+	}
+	if err := tx.Commit().Error; err != nil {
+		zap.L().Error("commit transaction failed", zap.Error(err))
+		return err
+	}
+	return nil
+}
+
+func (r *UserDB) GetUserByUserId(ctx context.Context, tx *gorm.DB, userId int64) (*model.UserDO, error) {
+	db := getDB(tx, r.DB)
 	var userDO model.UserDO
-	err := r.DB.WithContext(ctx).
+	err := db.WithContext(ctx).
 		Model(model.UserDO{}).
 		Where("id = ?", userId).
 		Scan(&userDO).Error
@@ -25,9 +58,10 @@ func (r *UserDB) GetUserByUserId(ctx context.Context, userId int64) (*model.User
 	return &userDO, nil
 }
 
-func (r *UserDB) GetUserByUserName(ctx context.Context, userName string) (*model.UserDO, error) {
+func (r *UserDB) GetUserByUserName(ctx context.Context, tx *gorm.DB, userName string) (*model.UserDO, error) {
+	db := getDB(tx, r.DB)
 	var userDO model.UserDO
-	err := r.DB.WithContext(ctx).
+	err := db.WithContext(ctx).
 		Model(model.UserDO{}).
 		Where("username = ?", userName).
 		Scan(&userDO).Error
@@ -38,9 +72,10 @@ func (r *UserDB) GetUserByUserName(ctx context.Context, userName string) (*model
 	return &userDO, nil
 }
 
-func (r *UserDB) GetUserNameById(ctx context.Context, userId int64) (string, error) {
+func (r *UserDB) GetUserNameById(ctx context.Context, tx *gorm.DB, userId int64) (string, error) {
+	db := getDB(tx, r.DB)
 	var userName string
-	err := r.DB.WithContext(ctx).
+	err := db.WithContext(ctx).
 		Model(model.UserDO{}).
 		Where("id = ?", userId).
 		Pluck("username", &userName).Error
@@ -51,9 +86,10 @@ func (r *UserDB) GetUserNameById(ctx context.Context, userId int64) (string, err
 	return userName, nil
 }
 
-func (r *UserDB) GetUserIdByUserName(ctx context.Context, userName string) (int64, error) {
+func (r *UserDB) GetUserIdByUserName(ctx context.Context, tx *gorm.DB, userName string) (int64, error) {
+	db := getDB(tx, r.DB)
 	var userId int64
-	err := r.DB.WithContext(ctx).
+	err := db.WithContext(ctx).
 		Model(model.UserDO{}).
 		Where("username = ?", userName).
 		Pluck("id", &userId).Error
@@ -64,21 +100,39 @@ func (r *UserDB) GetUserIdByUserName(ctx context.Context, userName string) (int6
 	return userId, nil
 }
 
-func (r *UserDB) UserExists(ctx context.Context, userName string) (bool, error) {
-	var userDO model.UserDO
-	tx := r.DB.WithContext(ctx).
+func (r *UserDB) UserExists(ctx context.Context, tx *gorm.DB, userName string) (bool, error) {
+	db := getDB(tx, r.DB)
+	var user model.UserDO
+	err := db.WithContext(ctx).
 		Model(model.UserDO{}).
 		Where("username = ?", userName).
-		Scan(&userDO)
-	if tx.Error != nil {
-		zap.L().Error(tx.Error.Error())
-		return false, tx.Error
+		First(&user).Error
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return false, nil
+		}
+		zap.L().Error("failed to check user existence", zap.Error(err))
+		return false, err
 	}
-	return tx.RowsAffected > 1, nil
+	return true, nil
 }
 
-func (r *UserDB) CreateUser(ctx context.Context, userDO *model.UserDO) error {
-	err := r.DB.WithContext(ctx).
+// func (r *UserDB) UserExists(ctx context.Context, userName string) (bool, error) {
+// 	var userDO model.UserDO
+// 	tx := r.DB.WithContext(ctx).
+// 		Model(model.UserDO{}).
+// 		Where("username = ?", userName).
+// 		Scan(&userDO)
+// 	if tx.Error != nil {
+// 		zap.L().Error(tx.Error.Error())
+// 		return false, tx.Error
+// 	}
+// 	return tx.RowsAffected > 1, nil
+// }
+
+func (r *UserDB) CreateUser(ctx context.Context, tx *gorm.DB, userDO *model.UserDO) error {
+	db := getDB(tx, r.DB)
+	err := db.WithContext(ctx).
 		Model(model.UserDO{}).
 		Create(userDO).Error
 	if err != nil {
@@ -87,9 +141,9 @@ func (r *UserDB) CreateUser(ctx context.Context, userDO *model.UserDO) error {
 	return err
 }
 
-func (r *UserDB) UpdateUser(ctx context.Context, updateUserMap map[string]interface{},
-	userId int64) error {
-	err := r.DB.WithContext(ctx).
+func (r *UserDB) UpdateUser(ctx context.Context, tx *gorm.DB, updateUserMap map[string]interface{}, userId int64) error {
+	db := getDB(tx, r.DB)
+	err := db.WithContext(ctx).
 		Model(model.UserDO{}).
 		Where("id = ?", userId).
 		Updates(updateUserMap).Error
@@ -99,8 +153,9 @@ func (r *UserDB) UpdateUser(ctx context.Context, updateUserMap map[string]interf
 	return err
 }
 
-func (r *UserDB) DeleteUser(ctx context.Context, userId int64) error {
-	err := r.DB.WithContext(ctx).
+func (r *UserDB) DeleteUser(ctx context.Context, tx *gorm.DB, userId int64) error {
+	db := getDB(tx, r.DB)
+	err := db.WithContext(ctx).
 		Model(model.UserDO{}).
 		Where("id = ?", userId).
 		Delete(model.UserDO{}).Error
@@ -114,22 +169,70 @@ type RepoDB struct {
 	DB *gorm.DB
 }
 
-func (r *RepoDB) RepoExists(ctx context.Context, repoName string, userId int64) (bool, error) {
-	var repoDO model.RepoDO
-	tx := r.DB.WithContext(ctx).
-		Model(model.RepoDO{}).
-		Where("repository_name = ? AND user_id = ?", repoName, userId).
-		Scan(&repoDO)
+func (r *RepoDB) WithTransaction(ctx context.Context, fn func(*gorm.DB) error) error {
+	tx := r.DB.Begin()
 	if tx.Error != nil {
-		zap.L().Error(tx.Error.Error())
-		return false, tx.Error
+		zap.L().Error("Try to get transaction connection failed", zap.Error(tx.Error))
+		return tx.Error
 	}
-	return tx.RowsAffected > 1, nil
+	defer func() {
+		if r := recover(); r != nil {
+			zap.L().Error("recover from panic",
+				zap.Any("panic", r),
+				zap.ByteString("stack", debug.Stack()),
+			)
+			tx.Rollback()
+			zap.L().Info("rollback successful")
+			panic(r)
+		}
+	}()
+	if err := fn(tx); err != nil {
+		zap.L().Error("transaction execute failed, rollback successful", zap.Error(err))
+		tx.Rollback()
+		zap.L().Info("rollback successful")
+		return err
+	}
+	if err := tx.Commit().Error; err != nil {
+		zap.L().Error("commit transaction failed", zap.Error(err))
+		return err
+	}
+	return nil
 }
 
-func (r *RepoDB) GetRepoIdByName(ctx context.Context, repoName string, userId int64) (int64, error) {
+func (r *RepoDB) RepoExists(ctx context.Context, tx *gorm.DB, repoName string, userId int64) (bool, error) {
+	db := getDB(tx, r.DB)
+	var repoDO model.RepoDO
+	err := db.WithContext(ctx).
+		Model(model.RepoDO{}).
+		Where("repository_name = ? AND user_id = ?", repoName, userId).
+		First(&repoDO).Error
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return false, nil
+		}
+		zap.L().Error("failed to check repository existence", zap.Error(err))
+		return false, err
+	}
+	return true, nil
+}
+
+// func (r *RepoDB) RepoExists(ctx context.Context, repoName string, userId int64) (bool, error) {
+// 	var repoDO model.RepoDO
+// 	tx := r.DB.WithContext(ctx).
+// 		Model(model.RepoDO{}).
+// 		Where("repository_name = ? AND user_id = ?", repoName, userId).
+// 		Scan(&repoDO)
+// 	if tx.Error != nil {
+// 		zap.L().Error(tx.Error.Error())
+// 		return false, tx.Error
+// 	}
+// 	return tx.RowsAffected > 1, nil
+// }
+
+func (r *RepoDB) GetRepoIdByName(ctx context.Context, tx *gorm.DB, repoName string, userId int64) (int64, error) {
+	db := getDB(tx, r.DB)
 	var repoId int64
-	err := r.DB.WithContext(ctx).
+	err := db.WithContext(ctx).
 		Model(&model.RepoDO{}).
 		Where("user_id = ? AND repository_name = ?", userId, repoName).
 		Pluck("id", &repoId).Error
@@ -139,9 +242,10 @@ func (r *RepoDB) GetRepoIdByName(ctx context.Context, repoName string, userId in
 	return repoId, err
 }
 
-func (r *RepoDB) GetRepoNameById(ctx context.Context, repoId int64) (string, error) {
+func (r *RepoDB) GetRepoNameById(ctx context.Context, tx *gorm.DB, repoId int64) (string, error) {
+	db := getDB(tx, r.DB)
 	var res string
-	err := r.DB.WithContext(ctx).
+	err := db.WithContext(ctx).
 		Model(&model.RepoDO{}).
 		Where("id = ?", repoId).
 		Select("repository_name").
@@ -153,8 +257,9 @@ func (r *RepoDB) GetRepoNameById(ctx context.Context, repoId int64) (string, err
 	return res, nil
 }
 
-func (r *RepoDB) CreateRepo(ctx context.Context, repo *model.RepoDO, userId int64) error {
-	err := r.DB.WithContext(ctx).
+func (r *RepoDB) CreateRepo(ctx context.Context, tx *gorm.DB, repo *model.RepoDO, userId int64) error {
+	db := getDB(tx, r.DB)
+	err := db.WithContext(ctx).
 		Model(model.RepoDO{}).
 		Create(repo).Error
 	if err != nil {
@@ -163,16 +268,18 @@ func (r *RepoDB) CreateRepo(ctx context.Context, repo *model.RepoDO, userId int6
 	return err
 }
 
-func (r *RepoDB) UpdateRepo(ctx context.Context, updateRepoMap map[string]interface{}, userId int64) error {
-	err := r.DB.WithContext(ctx).
+func (r *RepoDB) UpdateRepo(ctx context.Context, tx *gorm.DB, updateRepoMap map[string]interface{}, userId int64) error {
+	db := getDB(tx, r.DB)
+	err := db.WithContext(ctx).
 		Model(&model.RepoDO{}).
 		Updates(updateRepoMap).Error
 	return err
 }
 
-func (r *RepoDB) GetRepoByID(ctx context.Context, repoId int64) (*model.RepoDO, error) {
+func (r *RepoDB) GetRepoByID(ctx context.Context, tx *gorm.DB, repoId int64) (*model.RepoDO, error) {
+	db := getDB(tx, r.DB)
 	var repoDO model.RepoDO
-	err := r.DB.WithContext(ctx).
+	err := db.WithContext(ctx).
 		Model(&model.RepoDO{}).
 		Where("id = ?", repoId).
 		Scan(&repoDO).Error
@@ -187,8 +294,9 @@ type SshKeyDB struct {
 	DB *gorm.DB
 }
 
-func (r *SshKeyDB) Upload(ctx context.Context, sshKeyDO *model.SshKeyDO) error {
-	err := r.DB.WithContext(ctx).
+func (r *SshKeyDB) Upload(ctx context.Context, tx *gorm.DB, sshKeyDO *model.SshKeyDO) error {
+	db := getDB(tx, r.DB)
+	err := db.WithContext(ctx).
 		Model(&model.SshKeyDO{}).
 		Create(sshKeyDO).Error
 	if err != nil {
@@ -197,8 +305,9 @@ func (r *SshKeyDB) Upload(ctx context.Context, sshKeyDO *model.SshKeyDO) error {
 	return err
 }
 
-func (r *SshKeyDB) Update(ctx context.Context, updateSshKey map[string]interface{}, sshKeyId int64) error {
-	err := r.DB.WithContext(ctx).
+func (r *SshKeyDB) Update(ctx context.Context, tx *gorm.DB, updateSshKey map[string]interface{}, sshKeyId int64) error {
+	db := getDB(tx, r.DB)
+	err := db.WithContext(ctx).
 		Model(model.SshKeyDO{}).
 		Where("id = ?", sshKeyId).Error
 	if err != nil {
@@ -207,22 +316,72 @@ func (r *SshKeyDB) Update(ctx context.Context, updateSshKey map[string]interface
 	return err
 }
 
-func (r *SshKeyDB) SshKeyExists(ctx context.Context, sshKeyName string, userId int64) (bool, error) {
-	var sshKeyDO model.SshKeyDO
-	tx := r.DB.WithContext(ctx).
-		Model(&model.SshKeyDO{}).
-		Where("name = ? AND user_id = ?", sshKeyName, userId).
-		Scan(&sshKeyDO)
+func (r *SshKeyDB) WithTransaction(ctx, fn func(*gorm.DB) error) error {
+	tx := r.DB.Begin()
 	if tx.Error != nil {
-		zap.L().Error(tx.Error.Error())
-		return false, tx.Error
+		zap.L().Error("try to get transaction connection failed", zap.Error(tx.Error))
+		return tx.Error
 	}
-	return tx.RowsAffected > 1, tx.Error
+	defer func() {
+		if r := recover(); r != nil {
+			zap.L().Error("recover from panic",
+				zap.Any("panic", r),
+				zap.ByteString("stack", debug.Stack()),
+			)
+			tx.Rollback()
+			zap.L().Info("rollback successful")
+			panic(r)
+		}
+	}()
+	if err := fn(tx); err != nil {
+		zap.L().Error("transaction execute failed", zap.Error(err))
+		tx.Rollback()
+		zap.L().Info("rollback successful")
+		return err
+	}
+	if err := tx.Commit().Error; err != nil {
+		zap.L().Error("commit transaction failed", zap.Error(err))
+		tx.Rollback()
+		zap.L().Info("rollback successful")
+		return err
+	}
+	return nil
 }
 
-func (r *SshKeyDB) GetSshKeyIdBySshKeyName(ctx context.Context, sshKeyName string, userId int64) (int64, error) {
+func (r *SshKeyDB) SshKeyExists(ctx context.Context, tx *gorm.DB, sshKeyName string, userId int64) (bool, error) {
+	db := getDB(tx, r.DB)
+	var sshKeyDO model.SshKeyDO
+	err := db.WithContext(ctx).
+		Model(model.SshKeyDO{}).
+		Where("name = ? AND user_id = ?", sshKeyName, userId).
+		First(&sshKeyDO).Error
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return false, nil
+		}
+		zap.L().Error("failed to check user existence", zap.Error(err))
+		return false, err
+	}
+	return true, nil
+}
+
+// func (r *SshKeyDB) SshKeyExists(ctx context.Context, sshKeyName string, userId int64) (bool, error) {
+// 	var sshKeyDO model.SshKeyDO
+// 	tx := r.DB.WithContext(ctx).
+// 		Model(&model.SshKeyDO{}).
+// 		Where("name = ? AND user_id = ?", sshKeyName, userId).
+// 		Scan(&sshKeyDO)
+// 	if tx.Error != nil {
+// 		zap.L().Error(tx.Error.Error())
+// 		return false, tx.Error
+// 	}
+// 	return tx.RowsAffected > 1, tx.Error
+// }
+
+func (r *SshKeyDB) GetSshKeyIdBySshKeyName(ctx context.Context, tx *gorm.DB, sshKeyName string, userId int64) (int64, error) {
+	db := getDB(tx, r.DB)
 	var sshKeyId int64
-	err := r.DB.WithContext(ctx).
+	err := db.WithContext(ctx).
 		Model(model.SshKeyDO{}).
 		Where("user_id = ? AND name = ?", userId, sshKeyName).
 		Pluck("id", &sshKeyId).Error
@@ -232,9 +391,10 @@ func (r *SshKeyDB) GetSshKeyIdBySshKeyName(ctx context.Context, sshKeyName strin
 	return sshKeyId, err
 }
 
-func (r *SshKeyDB) GetSshKeyNameBySshKeyId(ctx context.Context, sshKeyId int64) (string, error) {
+func (r *SshKeyDB) GetSshKeyNameBySshKeyId(ctx context.Context, tx *gorm.DB, sshKeyId int64) (string, error) {
+	db := getDB(tx, r.DB)
 	var sshKeyName string
-	err := r.DB.WithContext(ctx).
+	err := db.WithContext(ctx).
 		Model(model.SshKeyDO{}).
 		Where("id = ?", sshKeyId).
 		Pluck("name", &sshKeyName).Error
@@ -244,8 +404,9 @@ func (r *SshKeyDB) GetSshKeyNameBySshKeyId(ctx context.Context, sshKeyId int64) 
 	return sshKeyName, err
 }
 
-func (r *SshKeyDB) DeleteSshKey(ctx context.Context, sshKeyName string, userId int64) error {
-	err := r.DB.WithContext(ctx).
+func (r *SshKeyDB) DeleteSshKey(ctx context.Context, tx *gorm.DB, sshKeyName string, userId int64) error {
+	db := getDB(tx, r.DB)
+	err := db.WithContext(ctx).
 		Model(model.SshKeyDO{}).
 		Where("user_id = ? AND name = ?", userId, sshKeyName).
 		Delete(model.SshKeyDO{}).Error
@@ -253,4 +414,11 @@ func (r *SshKeyDB) DeleteSshKey(ctx context.Context, sshKeyName string, userId i
 		zap.L().Error(err.Error())
 	}
 	return err
+}
+
+func getDB(tx *gorm.DB, fallback *gorm.DB) *gorm.DB {
+	if tx != nil {
+		return tx
+	}
+	return fallback
 }
